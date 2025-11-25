64800f29c3a9660736e1962e001e1ad7ada1dd72
Primovero1011 <fabio101173@googlemail.com>
2025-11-25 18:50:16 +0100

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      color: "white",
+      background: "#0B1120"
+    }}>
+      <h1>QR4You – coming soon</h1>
+    </div>
+  );
+}
